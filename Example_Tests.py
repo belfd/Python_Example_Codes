aef5dedf277cf79aa39f0518dc0d14cda282e232
belfd <belfd@yahoo.com>
2021-05-01 18:41:26 +0300

    Add Count vowels exercise

--- Example_Tests.py
+++ Example_Tests.py
@@ -7,15 +7,19 @@ vowel_count('awesome') # {'a': 1, 'e': 2, 'o': 1}
 vowel_count('Elie') # {'e': 2, 'i': 1}
 vowel_count('Colt') # {'o': 1}
 
-VOWELS=('a','e','o','i','u')
+# VOWELS=('a','e','o','i','u')
+#
+# def vowel_count(inp:str)->dict:
+#     dout = dict()
+#     for i in inp:
+#         if i in VOWELS:
+#             dout[i]=dout[i]+1 if i in dout else 1
+#
+#     return dout
 
-def vowel_count(inp:str)->dict:
-    dout = dict()
-    for i in inp:
-        if i in VOWELS:
-            dout[i]=dout[i]+1 if i in dout else 1
-
-    return dout
+def vowel_count(string):
+    lower_s = string.lower()
+    return {letter: lower_s.count(letter) for letter in lower_s if letter in "aeiou"}
 
 print(vowel_count('awesome'))
 print(vowel_count('Elie'))
