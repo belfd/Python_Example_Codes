987b74ba2ef78c8ab454e020d53fcb2cb1ffa8c1
belfd <belfd@yahoo.com>
2021-05-02 23:41:51 +0300

    two list dictionary

--- Example_Tests.py
+++ Example_Tests.py
@@ -72,3 +72,43 @@ def find_factors(num:int)->list[int]:
 print(find_factors(111))
 '''
 
+'''
+Write a function called 'two_list_dictionary' which accepts two lists of varying lengths.
+The first list consists of keys and the second one consists of values. 
+Your function should return a dictionary created from the keys and values. 
+If there are not enough values, the remaining keys should have a value of null. 
+If there not enough keys, just ignore the remaining values.
+two_list_dictionary(['a', 'b', 'c', 'd'], [1, 2, 3]) # {'a': 1, 'b': 2, 'c': 3, 'd': None}
+two_list_dictionary(['a', 'b', 'c']  , [1, 2, 3, 4]) # {'a': 1, 'b': 2, 'c': 3}
+two_list_dictionary(['x', 'y', 'z']  , [1,2]) # {'x': 1, 'y': 2, 'z': None}
+
+def two_list_dictionary(inp1,inp2):
+    if len(inp1) > len(inp2):
+        n=len(inp1)-len(inp2)
+        lst=[]
+        for i in range(n):
+            lst.append(None)
+        inp2=inp2+lst
+    result = zip(inp1,inp2)
+    dic = {}
+    for k,v in result:
+        dic.setdefault(k,[]).append(v)
+    return dic
+
+def two_list_dictionary(keys, values):
+    collection = {}
+
+    for idx, val in enumerate(keys):
+        if idx < len(values):
+            collection[keys[idx]] = values[idx]
+        else:
+            collection[keys[idx]] = None
+
+    return collection
+
+print(two_list_dictionary(['a', 'b', 'c', 'd'], [1, 2, 3]))
+print(two_list_dictionary(['a', 'b', 'c']  , [1, 2, 3, 4]))
+print(two_list_dictionary(['x', 'y', 'z']  , [1,2]))
+'''
+
+
