a8b9d16ecf3a835851dce6d284329eccea41bdb7
belfd <belfd@yahoo.com>
2021-05-09 01:16:01 +0300

    min max of keys in dictionary

--- Example_Tests.py
+++ Example_Tests.py
@@ -172,3 +172,19 @@ def find_the_duplicate(lst):
 
 print(find_the_duplicate([2,1,3,4]))
 '''
+
+'''
+Write a function called min_max_key_in_dictionary which returns a list with the lowest key in the dictionary 
+and the highest key in the dictionary. 
+You can assume that the dictionary will have keys that are numbers.
+
+min_max_key_in_dictionary({2:'a', 7:'b', 1:'c',10:'d',4:'e'}) # [1,10]
+min_max_key_in_dictionary({1: "Elie", 4:"Matt", 2: "Tim"}) # [1,4]
+
+
+def min_max_key_in_dictionary(inp):
+    lstmin,lstmax = min(inp.keys()),max(inp.keys())
+    return lstmin,lstmax
+
+print(min_max_key_in_dictionary({1: "Elie", 4:"Matt", 2: "Tim"}))
+'''
