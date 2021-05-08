78b8dae5abe2058e3c4c16f6270da4500a038cdd
belfd <belfd@yahoo.com>
2021-05-08 23:52:40 +0300

    count frequency of chars in number

--- Example_Tests.py
+++ Example_Tests.py
@@ -131,3 +131,27 @@ print(range_in_list([1,2,3,4])) # 10
 print(range_in_list([1,2,3,4],0,100)) # 10
 print(range_in_list([],0,1)) # 0
 '''
+
+'''
+Write a function called "same_frequency" which accepts two numbers and returns 
+True if they contain the same frequency of digits. 
+Otherwise, it returns False.
+
+same_frequency(551122,221515) # True
+same_frequency(321142,3212215) # False
+same_frequency(1212, 2211) # True
+
+
+def same_frequency(inp1,inp2):
+    d1 = {letter: str(inp1).count(letter) for letter in str(inp1)}
+    d2 = {letter: str(inp2).count(letter) for letter in str(inp2)}
+
+    for key, val in d1.items():
+        if not key in d2.keys():
+            return False
+        elif d2[key] != d1[key]:
+            return False
+    return True
+
+print(same_frequency(551122,221515)) #output: True
+'''
