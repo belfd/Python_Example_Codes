c2f3438b92d0fd1fa635b1ce05ca66b55fb0b6dd
belfd <belfd@yahoo.com>
2021-05-09 00:33:53 +0300

    find the duplicate

--- Example_Tests.py
+++ Example_Tests.py
@@ -155,3 +155,20 @@ def same_frequency(inp1,inp2):
 
 print(same_frequency(551122,221515)) #output: True
 '''
+
+'''
+Write a function called find_the_duplicate which accepts an array of numbers containing a single duplicate. 
+The function returns the number which is a duplicate or None if there are no duplicates.
+
+find_the_duplicate([1,2,1,4,3,12]) # 1
+find_the_duplicate([6,1,9,5,3,4,9]) # 9
+find_the_duplicate([2,1,3,4]) # None
+
+def find_the_duplicate(lst):
+    for letter in lst:
+        if lst.count(letter)>1:
+            return letter
+    return None
+
+print(find_the_duplicate([2,1,3,4]))
+'''
