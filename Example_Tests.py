2e138236bd6c201b7b9bbab094f379e35a958ea5
belfd <belfd@yahoo.com>
2021-10-27 01:25:43 +0300

    Update of 261021_1

--- Example_Tests.py
+++ Example_Tests.py
@@ -1,10 +1,70 @@
 
-from enum import Enum
-class ProgrammingLanguage(Enum):
-    PYTHON = "Python"
-    JAVA = "Java"
-    C = "C"
-    JAVASCRIPT = "JavaScript"
-    PHP = "Php"
+# What is a metaclass?
+# A metaclass is a class that defines the behavior of the classes and their instances.
+# A metaclass is a class whose instances are classes.
 
-print(f"The language I write is: {ProgrammingLanguage.PYTHON.value}")
+# How to create a class dynamically in python? using 'type' not metaclass
+def mymethod(self):
+    return self.x > 100
+
+class_name = "MyClass"
+base_classes = tuple()
+params= {"x": 10, "check_greater": mymethod}
+MyClass = type("MyClass", base_classes, params)  # 3 parts in init with type class - name,base_classes, attribs as dict
+obj = MyClass()
+print(obj.check_greater()) #output: False
+
+# A metaclass lets us to customize the creation of our user-defined classes before they are actually created!
+# In other words, it can add or remove attributes, methods or base classes to the actual class.
+# Metaclass is nothing but a class that derives from class type .
+#  Metaclass may override the methods__new__, __init__ or __call__ on class typeto provide customized behavior.
+
+# Lets say, I want to create a custom type of string which has method that says whether
+# the given string equals to zero. meaning "0000"
+# iszero method is not part of str type
+
+class StringFactory(type):
+    def iszero(self):
+        """Check if the given string is equals to one or more zeros"""
+        try:
+            return not sum([int(chr) for chr in self]) # when "0000" it returns True
+        except:
+            return False
+    def __new__(cls, name, bases, dct):
+        print("My class is not yet created")
+        bases += (str,)  # add str class as base to the new class
+        dct["iszero"] = StringFactory.iszero  # add our custom method to the new class
+        class_ = type.__new__(cls, name, bases, dct)
+        print("My class is created with custom method at run time")
+        return class_
+
+# define the metaclass
+class MyCustomString(metaclass=StringFactory):
+    pass
+
+test_string = MyCustomString("abcd")
+print(f"For 'abcd' it is: {test_string.iszero()} ")  # Outputs False
+test_string = MyCustomString("0000")
+print(f"For '0000' it is:{test_string.iszero()}")  # Outputs True
+
+# Assume that, we have to create a singleton class which creates only 1 object in
+# its lifetime irrespective of the number of times the class is instantiated.
+
+# Let us see how we can use the __call__ method in a metaclass.
+class MyMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in MyMeta._instances:
+            MyMeta._instances[cls] = super().__call__(*args, **kwargs)
+        return MyMeta._instances[cls]
+
+
+class Singleton(metaclass=MyMeta):
+    pass
+
+
+x = Singleton()
+y = Singleton()
+print(f"The example says x is y: {x is y}")  # Outputs True
+
--- Python_Examples.py
+++ Python_Examples.py
@@ -6651,6 +6651,84 @@ print(calc.add(2, 3))  # output: Calc.add is called with parameter (2, 3)  5
 print(calc.sub(2, 3))  # output: Calc.sub is called with parameter (2, 3) -1
 print(calc.mul(2, 3))  # output: Calc.mul is called with parameter (2, 3)  6
 
+
+# What is a metaclass?
+# A metaclass is a class that defines the behavior of the classes and their instances.
+# A metaclass is a class whose instances are classes.
+
+# How to create a class dynamically in python? using 'type' not metaclass
+def mymethod(self):
+    return self.x > 100
+
+class_name = "MyClass"
+base_classes = tuple()
+params= {"x": 10, "check_greater": mymethod}
+MyClass = type("MyClass", base_classes, params)  # 3 parts in init with type class - name,base_classes, attribs as dict
+obj = MyClass()
+print(obj.check_greater()) #output: False
+
+# A metaclass lets us to customize the creation of our user-defined classes before they are actually created!
+# In other words, it can add or remove attributes, methods or base classes to the actual class.
+# Metaclass is nothing but a class that derives from class type .
+#  Metaclass may override the methods__new__, __init__ or __call__ on class typeto provide customized behavior.
+
+# Lets say, I want to create a custom type of string which has method that says whether
+# the given string equals to zero. meaning "0000"
+# iszero method is not part of str type
+
+class StringFactory(type):
+    def iszero(self):
+        """Check if the given string is equals to one or more zeros"""
+        try:
+            return not sum([int(chr) for chr in self]) # when "0000" it returns True
+        except:
+            return False
+    def __new__(cls, name, bases, dct):
+        print("My class is not yet created")
+        bases += (str,)  # add str class as base to the new class
+        dct["iszero"] = StringFactory.iszero  # add our custom method to the new class
+        class_ = type.__new__(cls, name, bases, dct)
+        print("My class is created with custom method at run time")
+        return class_
+
+# define the metaclass
+class MyCustomString(metaclass=StringFactory):
+    pass
+
+test_string = MyCustomString("abcd")
+print(f"For 'abcd' it is: {test_string.iszero()} ")  # Outputs False
+test_string = MyCustomString("0000")
+print(f"For '0000' it is:{test_string.iszero()}")  # Outputs True
+
+# Assume that, we have to create a singleton class which creates only 1 object in
+# its lifetime irrespective of the number of times the class is instantiated.
+
+# Let us see how we can use the __call__ method in a metaclass.
+class MyMeta(type):
+    _instances = {}  # maintain a dictionary _instances in our metaclass MyMeta which holds
+                     # the reference between each class and its object.
+
+    def __call__(cls, *args, **kwargs):  # will be called each time a class is instantiated
+        if cls not in MyMeta._instances: # During first invocation, the class is not present in _instances dictionary
+            MyMeta._instances[cls] = super().__call__(*args, **kwargs) # We call the default __call__ method from type
+                                                                       # get the object and update our dictionary.
+        return MyMeta._instances[cls] # For further class instantiations,the same obj from _instances dict is returned.
+
+
+class Singleton(metaclass=MyMeta):
+    pass
+
+
+x = Singleton()
+y = Singleton()
+print(f"The example says x is y: {x is y}")  # Outputs True
+
+###################################################
+
+
+
+
+
 ### attrs ###
 print("===========================")
 print("######    attrs    ########")
