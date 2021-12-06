bbc9630693d0719bf245ba72846d174c5598c6fc
belfd <belfd@yahoo.com>
2021-12-06 10:46:06 +0200

    Update 061221

--- Example_Tests.py
+++ Example_Tests.py
@@ -1,9 +1,52 @@
-def func1():
-    x = 5
-    def func2():
-        nonlocal x
-        x += 6
-        print(x)
-    func2()
-    print(x)
-func1()
+# This findall() function returns a list of strings that are matched with the pattern.
+import re
+text = "Hello 12, how are you? This is 13"
+pattern = "\d+"
+result = re.findall(pattern, text)
+print(result) #Output: ['12', '13']
+
+# This search() method is used to search a particular pattern in a given text/string.
+# This method returns a match object if the matched pattern is found in the text. Otherwise, it returns None .
+
+import re
+text = "Python is a programming language"
+#cheking if 'Python' is present at the beginning
+match = re.search('\APython', text)
+if match:
+    print("Pattern found")
+else:
+    print("Pattern not found")
+
+# Output:Pattern found
+
+# This split() function splits the string whenever a pattern is matched and returns the list of strings where
+# the pattern has split the new string. This function takes two parameters, pattern and string, as input.
+# The first parameter is a pattern that denotes the regular expression.
+# Whenever the pattern is found in the string, it will split the string.
+
+import re
+text = "Hello 12, how are you? This is 13"
+pattern = "\d+"
+result = re.split(pattern, text)
+print(result) #Output: ['Hello ', ', how are you? This is ', '']
+
+# This sub() function is used to find a pattern in a string and then replace that pattern with some other text.
+# This function takes three parameters pattern, replace, string as input.
+
+import re
+text = "Namaste World! How are you doing?"
+#replacing whitespace characters with '!!'
+result = re.sub("\s", "!!", text)
+print(result) #Output: Namaste!!World!!!How!!are!!you!!doing?
+
+# This subn() method is almost similar to sub() method. The only difference is in the output of both methods.
+# The sub() method returns the string after replacing the matched pattern with replace parameter text,
+# and the subn() method returns a tuple that consists of a modified string along with the number of replacements
+# in the string.
+
+import re
+text = "Namaste world. I prefer to work at night"
+pattern = "wo"
+result = re.subn(pattern, "~", text)
+print(result) # Output: ('Namaste ~rld. I prefer to ~rk at night', 2)
+
--- Python_Examples.py
+++ Python_Examples.py
@@ -2637,6 +2637,62 @@ match = re.match(regex, text)              # Searches only at the beginning of t
 iter  = re.finditer(regex, text)           # Returns all occurrences as match objects.
 '''
 
+print("===================================")
+print(" ---- ====  REGEX ==== ------")
+
+# This findall() function returns a list of strings that are matched with the pattern.
+import re
+text = "Hello 12, how are you? This is 13"
+pattern = "\d+"
+result = re.findall(pattern, text)
+print(result) #Output: ['12', '13']
+
+# This search() method is used to search a particular pattern in a given text/string.
+# This method returns a match object if the matched pattern is found in the text. Otherwise, it returns None .
+
+import re
+text = "Python is a programming language"
+#cheking if 'Python' is present at the beginning
+match = re.search('\APython', text)
+if match:
+    print("Pattern found")
+else:
+    print("Pattern not found")
+
+# Output:Pattern found
+
+# This split() function splits the string whenever a pattern is matched and returns the list of strings where
+# the pattern has split the new string. This function takes two parameters, pattern and string, as input.
+# The first parameter is a pattern that denotes the regular expression.
+# Whenever the pattern is found in the string, it will split the string.
+
+import re
+text = "Hello 12, how are you? This is 13"
+pattern = "\d+"
+result = re.split(pattern, text)
+print(result) #Output: ['Hello ', ', how are you? This is ', '']
+
+# This sub() function is used to find a pattern in a string and then replace that pattern with some other text.
+# This function takes three parameters pattern, replace, string as input.
+
+import re
+text = "Namaste World! How are you doing?"
+#replacing whitespace characters with '!!'
+result = re.sub("\s", "!!", text)
+print(result) #Output: Namaste!!World!!!How!!are!!you!!doing?
+
+# This subn() method is almost similar to sub() method. The only difference is in the output of both methods.
+# The sub() method returns the string after replacing the matched pattern with replace parameter text,
+# and the subn() method returns a tuple that consists of a modified string along with the number of replacements
+# in the string.
+
+import re
+text = "Namaste world. I prefer to work at night"
+pattern = "wo"
+result = re.subn(pattern, "~", text)
+print(result) # Output: ('Namaste ~rld. I prefer to ~rk at night', 2)
+
+
 
 ### 5 Uses of Asterisks in Python ###
 # 1. Mathematics
@@ -7413,7 +7469,27 @@ print(f"The example says x is y: {x is y}")  # Outputs True
 
 ###################################################
 
+### Special Cases #########
+## Careful with chained operations
+print((False == False) in [False]) # makes sense False
+print(False == (False in [False])) # makes sense False
+print(False == False in [False]) # It is True because all comparison operations in Python have the same priority when
+# they’re chained together (i.e when you don’t use brackets) - so it becomes ((False == False) and (False in [False]))
 
+for x in range(7):
+    if x == 6:
+        print(x)  #print 6
+print(x) # print 6 - scope of x still leave after out of function
+
+def func():
+    try:
+        return 'from_try'
+    finally:
+        return 'from_finally'
+print(func()) #What will get printed? from_finally - because when try is returned also fianny is executed and it has
+# twice return statements - the last is overwriting the first
+
+###########################
 
 
 
