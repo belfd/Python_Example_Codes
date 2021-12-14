c063a2de0ae97e062004d332ec484cd85826efb9
belfd <belfd@yahoo.com>
2021-12-15 01:38:15 +0200

    Update 141221

--- Python_Examples.py
+++ Python_Examples.py
@@ -1488,6 +1488,35 @@ min_max_num = min_max(nums)  # min_max_num = (1, 3)
 # return values can be assigned into multiple variables using *tuple unpacking*
 min_num, max_num = min_max(nums)  # min_num = 1, max_num = 3
 
+# 6 ways to find the largest number in the list
+print("##### 6 ways to find largest nmber in list: #####")
+numbers_lst = [1, 2, 3, 4, 50, 6, 3, 2, 3, 8]
+# 1- Fast approach using built in max function
+print("Using Max Built-in function: ", max(numbers_lst))
+# 2- Manual Approach iterating all the elements of the list
+max_num = numbers_lst[0]
+for n in numbers_lst:
+    max_num = n if n >= max_num else max_num
+print("Manual Iteration: ", max_num)
+# 3- Using comprehensions, in this case for the list
+max_num = numbers_lst[0]
+[max_num := n for n in numbers_lst if n >= max_num]
+print("List Comprehension: ", max_num)
+# 4- Sort the list in ascending order and print the last element in the list.
+numbers_lst.sort()
+# printing the last element, which is in this case the largest one
+print("Using the sort list method:", numbers_lst[-1])
+# 5 - Using the built in sorted function and getting
+# the last list element afterwards
+sorted_lst = sorted(numbers_lst, reverse=True)
+max_num = sorted_lst[0]
+print("Sorted List: ", max_num)
+# 6 - Using the built-in reversed function to reverse
+# the list, convert it to a list and print first item
+print(list(reversed(numbers_lst))[0])
+# Another nice shortcut for this approach should be this:
+print(numbers_lst[::-1][0])
+
 
 # Chained function call
 def add(a, b):
@@ -6536,6 +6565,19 @@ i=0 and j=1
 i=1 and j=0
 i=1 and j=1
 '''
+
+## tee usage - Compare or apply some operation between each pair of adjacent elements
+from itertools import tee
+from typing import Iterable
+
+def window2(iterable: Iterable):
+  it, offset = tee(iter(iterable))
+  next(offset)
+  return zip(it, offset)
+
+l = [1, 2, 3, 4, 5, 6]
+window2(l)   # Is the same that: ((1,2), (2,3), (3, 4), ...)
+
 
 ## FizzBuzz example ##
 print("===========================")
