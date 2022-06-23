f3d06a258ba4ea2c88085f0b594ab51bb4a57ff5
belfd <belfd@yahoo.com>
2022-06-23 12:56:20 +0300

    Update 230622_1

--- Python_Examples.py
+++ Python_Examples.py
@@ -8924,6 +8924,49 @@ with open('some/path.json', 'w') as f:
     json.dump({'foo': 'bar'}, f)
 """
 
+'''
+JMESPath
+JMESPath is a JSON query language. 
+JMESPath in Python allows you to obtain the data you need from a JSON document or dictionary easily. 
+This library is available for Python, but also for many other programming languages, 
+meaning that if you master the JMESPath query language, you can use it in many places.
+
+Let’s start with some simple use-cases. We’ll fetch the first person from the array, 
+and then get the first person’s age:
+
+{
+  "persons": [
+    { "name": "erik", "age": 38 },
+    { "name": "john", "age": 45 },
+    { "name": "rob", "age": 14 }
+  ]
+}
+jmespath.search(‘persons[0]’, persons)
+# {‘name’: ‘erik’, ‘age’: 38}
+jmespath.search(‘persons[0].age’, persons)
+# 38
+
+In the problem statement above, we wanted to extract all the age fields from the array of persons in the JSON document. This JMESPath expression will get the job done:
+
+import jmespath
+persons = {
+    "persons": [
+    { "name": "erik", "age": 38 },
+    { "name": "john", "age": 45 },
+    { "name": "rob", "age": 14 }
+    ]
+}
+jmespath.search(‘persons[*].age’, persons)
+# [38, 45, 14]
+Suppose you want to filter the list, and only get the ages for people named ‘erik’. You can do so with a filter:
+
+jmespath.search(“persons[?name==’erik’].age”, persons)
+# [38]
+Note that we’re now using double quotes because we need to quote the name inside the filter expression.
+
+'''
+
+
 ###############################
 ### WORKING WITH CSV ##########
 '''
