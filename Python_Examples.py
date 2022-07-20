50f78f5dff13fb83a28bc37690f41c83ce16db15
belfd <belfd@yahoo.com>
2022-07-20 23:53:29 +0300

    Update 200722_4

--- Python_Examples.py
+++ Python_Examples.py
@@ -4125,6 +4125,35 @@ test()
 test()
 test()
 
+''''
+# Perform Flow Control on Function Inputs
+# Check NOT Node
+
+def check_not_None(func):
+  def check(x):
+    if x is not None:
+      return func(x)
+    else: 
+      return 'is None'
+  return check
+
+@check_not_None
+def f1(x):
+  return x**1
+
+@check_not_None
+def f2(x):
+  return x**2
+
+@check_not_None
+def f3(x):
+  return x**3
+
+print(f1(4))      # 4
+print(f2(None))   # 'is None'
+print(f3(4))      # 64
+'''
+
 #######################################
 ## CLASS DECORATOR EXPLAINED ##
 print("==========================")
