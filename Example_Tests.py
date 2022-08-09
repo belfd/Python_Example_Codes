3a3b8a4809ab5da672bb5fb425b0348c8e6f9c04
belfd <belfd@yahoo.com>
2022-08-09 23:13:47 +0300

    Update 090822_1

--- Example_Tests.py
+++ Example_Tests.py
@@ -1,4 +1,15 @@
+class Circle(object):
+    pi = 3.14 # class attribute
 
-string_list = ["hello", "world", "let's", "learn", "some", "Python"]
-result = max(string_list, key=len)
-print(result)
+    def __init__(self,radius_value):
+        self.radius = radius_value   # instance attribute
+        self.area = Circle.pi * self.radius ** 2  # instance attribute was not input from parameters
+        self.circumference = self.pi * 2 * self.radius  # instance attribute was not input from parameters
+
+
+my_circle = Circle(5)
+my_circle.color = 1  # attribute defined outside of init
+print(my_circle.radius)  # output: 5
+print(my_circle.pi)  # output: 3.14
+print(my_circle.color)  # output: 1
+print(my_circle.area)  # output: 78.5
--- Python_Examples.py
+++ Python_Examples.py
@@ -4828,6 +4828,28 @@ Properties are special kind of attributes which have getter, setter and delete m
 __get__, __set__ and __delete__ methods.
 However, there is a property decorator in Python which provides getter/setter access to an attribute Properties 
 are a special kind of attributes.
+attirbute can be of instance and defined in __init__ function
+attribute can be of instance and defined in other method
+attribute can class attribute and known in class level: 
+can be accessed as self.class_attribute inside the instance or <class_name>.class_attribute
+Example:
+
+class Circle(object):
+    pi = 3.14 # class attribute
+
+    def __init__(self,radius_value):
+        self.radius = radius_value   # instance attribute
+        self.area = Circle.pi * self.radius ** 2  # instance attribute was not input from parameters
+        self.circumference = self.pi * 2 * self.radius  # instance attribute was not input from parameters
+
+
+my_circle = Circle(5)
+my_circle.color = 1  # attribute defined outside of init
+print(my_circle.radius)  # output: 5
+print(my_circle.pi)  # output: 3.14
+print(my_circle.color)  # output: 1
+print(my_circle.area)  # output: 78.5
+
 '''
 
 
